bce71b1f64f720cc3894a111607d7ffc0c25fe00
Sebastian <sebastianlakla@gmail.com>
2023-11-28 20:25:54 +0100

    Add tabIndex at li items. Delete class lists add and remove. Coment class in css with change color

--- js/app.js
+++ js/app.js
@@ -5,21 +5,15 @@ const liLinkMonthly = document.querySelector('.box-person-links__item:nth-child(
 const hoursItems = document.querySelectorAll('.box-wrok-container__content:nth-child(2)');
 const main = function(){
     const daily = function(){
-        liLinkDaily.classList.add('change-link-color');
-        liLinkWeekly.classList.remove('change-link-color');
-        liLinkMonthly.classList.remove('change-link-color');
+      
         console.log('ok');
     }
     const weekly = function(){
-        liLinkWeekly.classList.add('change-link-color'); 
-        liLinkDaily.classList.remove('change-link-color');
-        liLinkMonthly.classList.remove('change-link-color');
+        
         console.log('good');
     }
     const monthly = function(){
-        liLinkMonthly.classList.add('change-link-color'); 
-        liLinkDaily.classList.remove('change-link-color');
-        liLinkWeekly.classList.remove('change-link-color');
+        
         console.log('great!');
     }
     const eventListeners = function(){
